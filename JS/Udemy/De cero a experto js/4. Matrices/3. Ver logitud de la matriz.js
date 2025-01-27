e02280cc88bebaad2ee956e28e2200008b6f05b0
Jhonnyer Otalvaro <76260612+Jhonnyer-O@users.noreply.github.com>
2025-01-27 16:31:51 -0500

    Verificacion de columnas y fila de matriz

---
+++ JS/Udemy/De cero a experto js/4. Matrices/3. Ver logitud de la matriz.js
@@ -0,0 +1,31 @@
+
+
+let matriz = [[100,200,300],[400,500,600, 1000]];
+
+//Validar los reglones:
+
+console.log(matriz.length); // = 2
+
+//Calidar numero de columnas:
+console.log(matriz[0].length); // = 3
+console.log(matriz[1].length); // = 4
+
+
+/*
+
+Se entiende mejor de la siguiente manera:
+
+
+let matriz =    Columnas
+       Filas    0 - 1 - 2  - 3
+		0 =	  [[100,200,300],
+		1 =	  [400,500,600,1000]];
+
+
+
+
+
+
+
+
+*/
