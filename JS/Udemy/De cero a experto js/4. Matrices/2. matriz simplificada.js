37141b892aff43844550eedba9c88c429c8958c5
Jhonnyer Otalvaro <76260612+Jhonnyer-O@users.noreply.github.com>
2025-01-27 16:14:16 -0500

    Matriz simplificada

---
+++ JS/Udemy/De cero a experto js/4. Matrices/2. matriz simplificada.js
@@ -0,0 +1,21 @@
+//Declaracion de una matriz
+
+let matriz = [[100,200, 300],[400,500,600]];
+
+//agregando valores
+
+//Primer arreglo = renglones (hacia abajo) - segunda arreglo = columnas (Hacia la derecha)
+
+
+//----->
+matriz[0][0] = 100;
+matriz[0][1] = 200;
+matriz[0][2] = 300;
+
+//hacia abajo
+matriz[1][0] = 400;
+matriz[1][1] = 500;
+matriz[1][2] = 600;
+
+//Leer valores
+console.log('Elemento en [0][2] = '+matriz[0][1]);
