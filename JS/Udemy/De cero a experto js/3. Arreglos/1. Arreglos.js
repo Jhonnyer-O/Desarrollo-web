6537e82cd2aaba7fb70130ff2a508f7a18da82d2
Jhonnyer Otalvaro <76260612+Jhonnyer-O@users.noreply.github.com>
2025-01-27 12:13:48 -0500

    Arreglos en js

---
+++ JS/Udemy/De cero a experto js/3. Arreglos/1. Arreglos.js
@@ -0,0 +1,15 @@
+
+
+let numerosArreglo = [];
+
+// Modificar los valores o agregarlos
+
+numerosArreglo[0] = 13;
+numerosArreglo[1] = 20;
+numerosArreglo[4] = 'Maria';
+
+//imprimir valores:
+
+console.log(' Elemento 1 en [0]: '+numerosArreglo[0]);
+console.log(' Elemento 2 en [2]: '+numerosArreglo[2]);
+console.log(' Elemento 5 en [4]: '+numerosArreglo[4]);
