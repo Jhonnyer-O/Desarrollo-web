6e65122696ceca245a2824a421b4234f96489da1
Jhonnyer Otalvaro <76260612+Jhonnyer-O@users.noreply.github.com>
2025-01-28 08:19:18 -0500

    La estructura verdadera de una funcion

---
+++ JS/Udemy/De cero a experto js/5. Funciones/3. Funciones en js.js
@@ -0,0 +1,11 @@
+//1. Declarar la funcion
+function sumar(a,b){
+	let resultado = a+b;
+	return resultado;
+}
+
+//Llamar o invocar la funcion.
+let num1 = 5, num2 = 10;
+let r = sumar(num1,num2);
+console.log(r);
+
