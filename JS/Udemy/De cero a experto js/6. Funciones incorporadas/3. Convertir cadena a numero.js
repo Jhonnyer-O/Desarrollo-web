28a8c996c896aa326e50e840c1b289cecfa3845c
Jhonnyer Otalvaro <76260612+Jhonnyer-O@users.noreply.github.com>
2025-01-28 14:49:10 -0500

    Convertir un string a numero

---
+++ JS/Udemy/De cero a experto js/6. Funciones incorporadas/3. Convertir cadena a numero.js
@@ -0,0 +1,9 @@
+//convertir de cadena a numero
+
+let a= '10', b = '20';
+//como son cadenas se concatenan
+console.log(a+b); // = 1020
+
+//Convertir a numeros para que se sumen
+let suma = parseInt(a) + parseInt(b);
+console.log(suma);
