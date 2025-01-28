67958e0e8f87fc707959ce2356845e887d5d189a
Jhonnyer Otalvaro <76260612+Jhonnyer-O@users.noreply.github.com>
2025-01-28 14:31:48 -0500

    Subcadenas con funcion substring

---
+++ JS/Udemy/De cero a experto js/6. Funciones incorporadas/2. Subcadenas.js
@@ -0,0 +1,12 @@
+//Subcadenas
+
+let cadena3 = 'Hola Mundo';
+
+//Substring(indice_inicio, indice_fin - 1)
+
+let cadena1 = cadena3.substring(0,4);
+console.log(cadena1);
+
+
+let cadena2 = cadena3.substring(5,10);
+console.log(cadena2);
