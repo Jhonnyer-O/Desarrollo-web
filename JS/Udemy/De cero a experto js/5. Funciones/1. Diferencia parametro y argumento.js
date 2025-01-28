3023402cfc5ad01e1b5d53bbd56bf9905e0c16d0
Jhonnyer Otalvaro <76260612+Jhonnyer-O@users.noreply.github.com>
2025-01-27 20:08:51 -0500

    Diferencia entre argumento y parametro

---
+++ JS/Udemy/De cero a experto js/5. Funciones/1. Diferencia parametro y argumento.js
@@ -0,0 +1,16 @@
+
+//funcion como parametro:
+//Se declara la funcion
+function imprimir(num){
+
+  console.log(num);
+
+}
+//Llamada de la funcion
+
+//con argumento "Varieble"
+let variable = 5;
+imprimir(variable);
+
+
+
