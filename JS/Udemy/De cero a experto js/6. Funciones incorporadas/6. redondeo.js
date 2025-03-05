5f23c6510f7f41454e41749bf26989c8e55b996b
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2025-03-04 20:02:06 -0500

    Se añaden los metodos de redondeo y truncado en js

---
+++ JS/Udemy/De cero a experto js/6. Funciones incorporadas/6. redondeo.js
@@ -0,0 +1,15 @@
+// metodo de redondeo y truncado 
+
+let numero = 8.5, redondeo, truncado;
+
+//redondeo
+//Math.round() redondea el valor entero  mas cercano
+
+redondeo = Math.round(numero);
+console.log(redondeo);
+
+//El truncado 
+//Elimina el numero flotante Math.trunc()
+
+truncado = Math.trunc(numero);
+console.log(truncado);
