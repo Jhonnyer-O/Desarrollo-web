cf9f9b9fe9020e1fbd244a1068552ae83c711103
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2025-03-04 20:15:22 -0500

    Creacion de objetos en js, y acceder a ellos

---
+++ JS/Udemy/De cero a experto js/7. Objetos en js/1. objetos en js.js
@@ -0,0 +1,20 @@
+// Los objetos se crean de la siguiente manera:
+//aL crear un obbjeto se almacena un espacio en la memoria. que contiene una referencia
+
+let persona = {
+    nombre: 'Maria',
+    apellido: 'Murillo',
+    correo: 'Isamaria@mail.com',
+    edad: 20
+}
+
+// De la siguiente manera se puede acceder al valor del objeto:
+
+console.log(persona.nombre);
+console.log(persona.apellido);
+console.log(persona.correo);
+console.log(persona.edad);
+
+// Y de esta forma se accede al objeto con la informacion completa
+
+console.log(persona);
