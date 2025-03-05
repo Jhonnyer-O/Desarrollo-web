2b7f29b309d385375430f7d2bdda6b12464d85c2
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2025-03-05 18:21:51 -0500

    Se le agrego metodo para imprimir el nombre completo concatenado

--- JS/Udemy/De cero a experto js/7. Objetos en js/1. objetos en js.js
+++ JS/Udemy/De cero a experto js/7. Objetos en js/1. objetos en js.js
@@ -5,7 +5,13 @@ let persona = {
     nombre: 'Maria',
     apellido: 'Murillo',
     correo: 'Isamaria@mail.com',
-    edad: 20
+    edad: 20,
+
+    //Se le agrega una funcion  para concatenar el nombre completo
+    nombreCompleto: function(){
+        return this.nombre + ' ' + this.apellido;
+    }
+
 }
 
 // De la siguiente manera se puede acceder al valor del objeto:
@@ -18,3 +24,7 @@ console.log(persona.edad);
 // Y de esta forma se accede al objeto con la informacion completa
 
 console.log(persona);
+
+
+//Se llama a la funcion
+console.log(persona.nombreCompleto());
