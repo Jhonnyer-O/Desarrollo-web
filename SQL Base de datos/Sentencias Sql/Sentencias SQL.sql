a706421d43f4dbde16b83b4db433e6e8a6fa660e
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2025-07-31 19:32:22 -0500

    Agregando sentencias SQL

--- SQL Base de datos/Sentencias Sql/Sentencias SQL.sql
+++ SQL Base de datos/Sentencias Sql/Sentencias SQL.sql
@@ -1,8 +1,23 @@
--- Crear una base de datos:
+--Crear una base de datos:
+CREATE DATABASE Nombre_del_BD;
 
-CREATE DATABASE (Nombre del BD);
-
--- Ver las bases de datos creadas:
-
+--Ver las bases de datos creadas:
 SHOW DATABASES;
 
+--Ingreso a una base de datos:]
+USE Nombre_del_BD;
+
+--Creacion de una tabla de datos:
+CREATE TABLE Nombre_de_Tabla (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    email VARCHAR(100),
+    telefono VARCHAR(20)
+);
+
+--Insetar datos dentro de la tabla:
+INSERT INTO Nombre_de_Tabla (nombre, email, telefono)
+VALUES ('Juan Pérez', 'juanp@gmail.com', '3001234567');
+
+--Consulta de datos de la tabla:
+SELECT * FROM Nombre_de_Tabla;
