eb5df549114dbf8afc8d71c951bbed0e7869ed21
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2025-08-15 18:41:23 -0500

    Ciclo for js

---
+++ JS/Curso profesional Js/Fundamentos/4. Ciclos.js
@@ -0,0 +1,13 @@
+let i = 0;
+
+for(let i = 1; i <= 10; i++){
+    console.log(i + ' Hola');
+}
+
+for(let i = 10; i >= 1; i--){
+    console.log(i + ' menos');
+}
+
+for(let i = 1, j=2; i < 400; i*=j, j *=j){
+    console.log(i,j);
+}
