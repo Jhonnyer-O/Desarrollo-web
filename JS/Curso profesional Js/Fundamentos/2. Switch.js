f7fa1cef10366875b87b60fee641807a050a41ff
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2025-08-15 18:27:08 -0500

    Aprendiendo switch en js

---
+++ JS/Curso profesional Js/Fundamentos/2. Switch.js
@@ -0,0 +1,17 @@
+let objeto = "animal";
+switch(objeto){
+    case "enemigo":
+        console.log("Atacar");
+        break;
+    
+    case "piedra":
+        console.log("caer");
+        break;
+
+    case "animal":
+        console.log("Cazar");
+    break;
+    
+    default: 
+        console.log("No hay accion");
+}
