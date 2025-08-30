941e23d3293a25f6527ad872b2e7cbcebd6629d5
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2025-08-30 18:19:04 -0500

    Agregando patron modular, evento clic listener

--- JS/DOM/Curso udemy/Figuras-geometricas/js/geometric.js
+++ JS/DOM/Curso udemy/Figuras-geometricas/js/geometric.js
@@ -1,83 +1,100 @@
-//funcion que calcula el area y perimetro del cuadrado
-const perimSquare = (side) => side * 4;
-const areaSquare = (side) => side * side;
+const myModule = (() => {
+  "use strict";
 
-//funcion que calcula perimetro y area de un triangulo
-const perimTriangle = (side1, side2, base) => side1 + side2 + base;
-const areaTriangle = (base, height) => (base * height) / 2;
+  const valueResult = document.getElementById("result");
 
-//área, perímetro y diametro de circulo 
- const diameCircle = (radio)=>radio*2;
-const perimCircle = (radio) => diameCircle(radio) * Math.PI;
-const areaCircle = (radio) => (radio * radio) * Math.PI;
+  const btnPerimSquare = document.querySelector("#btnPerimSquare"),
+        btnAreaSquare = document.querySelector("#btnAreaSquare"),
+        btnPerimTriangle = document.querySelector("#btnPerimTriangle"),
+        btnAeaTriangle = document.querySelector("#btnAeaTriangle"),
+        btnDiameCircle = document.querySelector("#btnDiameCircle"),
+        btnPerimCircle = document.querySelector("#btnPerimCircle"),
+        btnAreaCircle = document.querySelector("#btnAreaCircle");
 
+  //funcion que calcula el area y perimetro del cuadrado
+  const perimSquare = (side) => side * 4;
+  const areaSquare = (side) => side * side;
 
-function calcularPerimSquare(){
-    let inputSide = document.getElementById('inputSide');
+  //funcion que calcula perimetro y area de un triangulo
+  const perimTriangle = (side1, side2, base) => side1 + side2 + base;
+  const areaTriangle = (base, height) => (base * height) / 2;
+
+  //área, perímetro y diametro de circulo
+  const diameCircle = (radio) => radio * 2;
+  const perimCircle = (radio) => diameCircle(radio) * Math.PI;
+  const areaCircle = (radio) => radio * radio * Math.PI;
+
+  btnPerimSquare.addEventListener('click',()=>{
+    let inputSide = document.getElementById("inputSide");
     let value = Number(inputSide.value);
 
     const result = `Èl perimetro del Cuadrado es: ${perimSquare(value)} CM`;
-    document.getElementById('result').innerText = result;
-}
+    valueResult.innerText = result;
+  });
 
-function calcularAreaSquare(){
-    let inputSide = document.getElementById('inputSide');
+  //function calcularAreaSquare() {
+  btnAreaSquare.addEventListener('click',()=>{
+    let inputSide = document.getElementById("inputSide");
     let value = Number(inputSide.value);
 
     const result = `Èl area del Cuadrado es: ${areaSquare(value)} CM^2`;
-    document.getElementById('result').innerText = result;
-}
+    //document.getElementById("result").innerText = result;
+    valueResult.innerText = result;
+  });
 
-function calcularPerimetroTriangulo(){
-    let input1 = document.getElementById('input1');
-    let input2 = document.getElementById('input2');
-    let input3 = document.getElementById('input3');
+  btnPerimTriangle.addEventListener('click',()=>{
+    let input1 = document.getElementById("input1");
+    let input2 = document.getElementById("input2");
+    let input3 = document.getElementById("input3");
 
     let value1 = Number(input1.value);
     let value2 = Number(input2.value);
     let value3 = Number(input3.value);
 
-    const result = `El perimetro del triangulo es: ${perimTriangle(value1,value2,value3)}`;
-    document.getElementById('result').innerText = result;
-}
+    const result = `El perimetro del triangulo es: ${perimTriangle(
+      value1,
+      value2,
+      value3
+    )}`;
+    valueResult.innerText = result;
+  });
 
-function calcularAreaTriangulo(){
-    let input1 = document.getElementById('side1');
-    let input2 = document.getElementById('side2');
+  btnAeaTriangle.addEventListener('click',()=> {
+    let input1 = document.getElementById("side1");
+    let input2 = document.getElementById("side2");
 
     let value1 = Number(input1.value);
     let value2 = Number(input2.value);
-    
-    const result = `El perimetro del triangulo es: ${areaTriangle(value1,value2)}`;
-    document.getElementById('result').innerText = result;
-}
 
-function calcularDiametro(){
+    const result = `El perimetro del triangulo es: ${areaTriangle(
+      value1,
+      value2
+    )}`;
+    valueResult.innerText = result;
+  });
 
-    let input1 = document.getElementById('sideR');
+  btnDiameCircle.addEventListener('click',()=> {
+    let input1 = document.getElementById("sideR");
     let value1 = Number(input1.value);
 
     const result = `El Diametro del Circulo es: ${diameCircle(value1)}`;
-    document.getElementById('result').innerText = result;
+    valueResult.innerText = result;
+  });
 
-}
-
-function calcularPerimetro(){
-
-    let input1 = document.getElementById('sideR');
+  btnPerimCircle.addEventListener('click',()=> {
+    let input1 = document.getElementById("sideR");
     let value1 = Number(input1.value);
 
     const result = `El Perimetro del Circulo es: ${perimCircle(value1)}`;
-    document.getElementById('result').innerText = result;
+    valueResult.innerText = result;
+  });
 
-}
-
-function calcularArea(){
-
-    let input1 = document.getElementById('sideR');
+  btnAreaCircle.addEventListener('click',()=> {
+    let input1 = document.getElementById("sideR");
     let value1 = Number(input1.value);
 
     const result = `El Area del Circulo es: ${areaCircle(value1)}`;
-    document.getElementById('result').innerText = result;
+    valueResult.innerText = result;
+  });
 
-}
+})();
