1ed2ead2d845800e54500687772d4bf1c8ff2f1b
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2026-03-10 20:39:31 -0500

    Ejercicio mayor de edad cine

---
+++ JS/Ejercicios con IA/2. Condicionales/ejercicio5.js
@@ -0,0 +1,16 @@
+let edad = 18;
+let dinero = 15000;
+let tieneEntrada = false;
+
+if (edad < 18) {
+    console.log("Eres menor de edad, no puedes entrar");
+}
+else if (tieneEntrada) {
+    console.log("Bienvenido al cine");
+}
+else if (dinero >= 12000) {
+    console.log("Puedes comprar la entrada");
+}
+else {
+    console.log("No tienes dinero suficiente");
+}
