c68520ef7b710123803cad3bcebddc9bcd54f946
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2026-03-10 20:48:50 -0500

    Ejercicio de sistema de descuentos

--- JS/Ejercicios con IA/2. Condicionales/ejercicio9.js
+++ JS/Ejercicios con IA/2. Condicionales/ejercicio9.js
@@ -1,18 +1,25 @@
 let compra = 50;
+let total;
 
-if(compra >= 200){
-  Desc = compra - (compra * 0.20)
+if (compra >= 200){
+    total = compra - (compra * 0.20);
     console.log(`Tu compra fue de: ${compra}
-        Descuento aplicado: 20%
-        Total a pagar: ${Desc}`);
-}else if(compra >= 100){
-    Desc = compra - (compra * 0.10);
+Descuento aplicado: 20%
+Total a pagar: ${total}`);
+}
+else if (compra >= 100){
+    total = compra - (compra * 0.10);
     console.log(`Tu compra fue de: ${compra}
-        Descuento aplicado: 10%
-        Total a pagar: ${Desc}`);
-}else{
-    Desc = compra - (compra * 0.05)
+Descuento aplicado: 10%
+Total a pagar: ${total}`);
+}
+else if (compra >= 50){
+    total = compra - (compra * 0.05);
     console.log(`Tu compra fue de: ${compra}
-        Descuento aplicado: 5%
-        Total a pagar: ${Desc}`);
+Descuento aplicado: 5%
+Total a pagar: ${total}`);
+}
+else{
+    console.log(`Tu compra fue de: ${compra}
+No tienes descuento`);
 }
