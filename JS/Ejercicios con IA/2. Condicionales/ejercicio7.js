8d9dabc98a436c00a19b6207b13ddf3aa9acc33a
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2026-03-10 20:44:54 -0500

    nota

---
+++ JS/Ejercicios con IA/2. Condicionales/ejercicio7.js
@@ -0,0 +1,14 @@
+let nota = 32;
+
+if (nota >= 90) {
+    console.log(`Tu resultado es: Excelente con una nota de ${nota}`);
+}
+else if (nota >= 70) {
+    console.log(`Tu resultado es: Aprobado con una nota de ${nota}`);
+}
+else if (nota >= 50) {
+    console.log(`Tu resultado es: Recuperación con una nota de ${nota}`);
+}
+else {
+    console.log(`Tu resultado es: Reprobado con una nota de ${nota}`);
+}
