222f734e2b4b1f35d6e08125e326d08e89ad22ac
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2026-03-11 20:18:15 -0500

    ejemplo switch elegir comida

---
+++ JS/Ejercicios con IA/3.Switch/ejemplo1.js
@@ -0,0 +1,24 @@
+let opcion = 2;
+
+switch(opcion){
+
+    case 1:
+        console.log("Elegiste Hamburguesa");
+        break;
+
+    case 2:
+        console.log("Elegiste Pizza");
+        break;
+
+    case 3:
+        console.log("Elegiste Tacos");
+        break;
+
+    case 4:
+        console.log("Elegiste Ensalada");
+        break;
+
+    default:
+        console.log("Opción no válida");
+
+}
