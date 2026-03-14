e7d88acc19fb93432c0cc99cd16392d25ada3948
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2026-03-13 19:36:40 -0500

    Ejercicio Switch seleccionar que operacion

---
+++ JS/Ejercicios con IA/3.Switch/2ejercicio.js
@@ -0,0 +1,25 @@
+switch (ope){
+    
+    case "-":   
+        calc = num1 - num2;
+        console.log(`La resta es: ${calc}`);
+        break;
+
+    case "+":   
+        calc = num1 + num2;
+        console.log(`La suma es: ${calc}`);
+        break;
+
+    case "*":   
+        calc = num1 * num2;
+        console.log(`La multiplicación es: ${calc}`);
+        break;
+
+    case "/":   
+        calc = num1 / num2;
+        console.log(`La división es: ${calc}`);
+        break;
+
+    default:
+        console.log("Operación no válida");
+}
