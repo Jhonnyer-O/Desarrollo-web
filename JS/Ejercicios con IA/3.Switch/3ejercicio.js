e9a6bb343a767de9c59e274ec1e4c727ea2ba8b9
Jhonnyer-O~ <Jhonnyer.o95@gmail.com>
2026-03-13 20:07:07 -0500

    Ejercicio de switch

---
+++ JS/Ejercicios con IA/3.Switch/3ejercicio.js
@@ -0,0 +1,27 @@
+let opcion = 3;
+
+switch (opcion){
+    
+    case 1:   
+        
+        console.log(`Has elegido: Ver perfil`);
+        break;
+
+    case 2:   
+        
+        console.log(`Has elegido: Editar perfil`);
+        break;
+
+    case 3:   
+        
+        console.log(`Has elegido: Cambiar contraseña`);
+        break;
+
+    case 4:   
+        
+        console.log(`Has elegido: Cerrar Sesion`);
+        break;
+
+    default:
+        console.log("Operación no válida");
+}
